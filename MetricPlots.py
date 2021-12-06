44a6d53b21ffa74525a2e3cd2f7f402df4a501b2
jgatter <j.gatter@student.tue.nl>
2021-12-06 10:19:45 +0100

    edited evaluation plots

--- MetricPlots.py
+++ MetricPlots.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import size
 
@@ -25,20 +26,20 @@ def precision_plot(dict):
         medium = df[df['Value'].str.contains("mAP") & df['Value'].str.contains("(medium)")].t
         large = df[df['Value'].str.contains("mAP") & df['Value'].str.contains("(large)")].t
         # subplot
-        ax1.plot(1,mAP, marker="o",label=model)
+        ax1.plot(1,mAP, marker=".",label=model)
         ax1.title.set_text('mAP')
-        ax2.plot(1,mAP50IOU, marker="o",label=model)
+        ax2.plot(1,mAP50IOU, marker=".",label=model)
         ax2.title.set_text('mAP @ 0.50 IoU')
-        ax3.plot(1,mAP75IOU, marker="o",label=model)
+        ax3.plot(1,mAP75IOU, marker=".",label=model)
         ax3.title.set_text('mAP @ 0.75 IoU')
-        ax4.plot(1,small, marker="o",label=model)
+        ax4.plot(1,small, marker=".",label=model)
         ax4.title.set_text('mAP (small)')
-        ax5.plot(1,medium, marker="o",label=model)
+        ax5.plot(1,medium, marker=".",label=model)
         ax5.title.set_text('mAP (medium)')
-        ax6.plot(1,large, marker="o",label=model)
+        ax6.plot(1,large, marker=".",label=model)
         ax6.title.set_text('mAP (large)')
     #plt.legend(loc=(1.04, 0))
-    plt.legend(loc='lower center', bbox_to_anchor=(-0.7, -0.5), ncol=3)
+    plt.legend(loc='lower center', bbox_to_anchor=(-0.7, -0.6), ncol=3, fontsize = 'xx-small')
     prec_fig.subplots_adjust(bottom=0.2)
     #plt.show()
     return prec_fig
@@ -60,20 +61,45 @@ def recall_plot(dict):
         medium = df[df['Value'].str.contains("AR@100") & df['Value'].str.contains("(medium)")].t
         large = df[df['Value'].str.contains("AR@100") & df['Value'].str.contains("(large)")].t
         # subplot
-        ax1.plot(1,recAR1, marker="o",label=model)
+        ax1.plot(1,recAR1, marker=".",label=model)
         ax1.title.set_text('AR @ 1')
-        ax2.plot(1,recAR10, marker="o",label=model)
+        ax2.plot(1,recAR10, marker=".",label=model)
         ax2.title.set_text('AR @ 10')
-        ax3.plot(1,recAR100, marker="o",label=model)
+        ax3.plot(1,recAR100, marker=".",label=model)
         ax3.title.set_text('AR @ 100')
-        ax4.plot(1,small, marker="o",label=model)
+        ax4.plot(1,small, marker=".",label=model)
         ax4.title.set_text('AR @ 100 (small)')
-        ax5.plot(1,medium, marker="o",label=model)
+        ax5.plot(1,medium, marker=".",label=model)
         ax5.title.set_text('AR @ 100 (medium)')
-        ax6.plot(1,large, marker="o",label=model)
+        ax6.plot(1,large, marker=".",label=model)
         ax6.title.set_text('AR @ 100 (large)')
     #plt.legend(loc=(1.04, 0))
-    plt.legend(loc='lower center', bbox_to_anchor=(-0.7, -0.5), ncol=3)
+    plt.legend(loc='lower center', bbox_to_anchor=(-0.7, -0.6), ncol=3, fontsize = 'xx-small')
     rec_fig.subplots_adjust(bottom=0.2)
     #plt.show()
     return rec_fig
+
+def plot_total_loss(loss_df,model_path):
+    """Plot total loss (from training event file)
+    tot_loss=f(time_step)"""
+    plt.plot(loss_df.Step, loss_df.t)
+    plt.xlabel('Step')
+    plt.ylabel('Total loss')
+    # get model name (last part of model path)
+    model = os.path.basename(os.path.normpath(model_path))
+    plt.savefig(os.path.join(model_path,f'TotLoss_{model}.png'))
+    plt.close()
+    print(f'Total loss plot saved in {model_path}')
+
+def plot_learningrate(lrate_df,model_path):
+    """Plot learning rate (from training event file)
+    lr=f(time_step)"""
+    plt.plot(lrate_df.Step, lrate_df.t)
+    plt.xlabel('Step')
+    plt.ylabel('Learning rate')
+    # get model name (last part of model path)
+    model = os.path.basename(os.path.normpath(model_path))
+    plt.savefig(os.path.join(model_path,f'LearningRate_{model}.png'))
+    plt.close()
+    print(f'Learning rate plot saved in {model_path}')
+    
--- Models_Evaluations.py
+++ Models_Evaluations.py
@@ -6,25 +6,35 @@ from tensorboard import program
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from openpyxl import load_workbook
 from tensorflow.python.framework import tensor_util
 from IPython.display import display
 
-from MetricPlots import precision_plot, recall_plot
+from MetricPlots import precision_plot, recall_plot, plot_total_loss, plot_learningrate
 from tidecv import TIDE, datasets
 
 """Get properties that are available in Tensorboard for custom Object Detection models"""
 
 # set model name
-custom_models = ['my_faster_rcnn_resnet101_v1_1024']
-                #'my_ssd_mobnet',
-                #'my_faster_rcnn_resnet50',
-                #'my_faster_rcnn_resnet50_2',
-                #'my_faster_rcnn_resnet50_3',
-                #'my_faster_rcnn_resnet50_4',
-                #'my_faster_rcnn_resnet50_5']
+custom_models = [#'my_faster_rcnn_resnet101_v1_640',
+                'my_faster_rcnn_resnet101_v1_1024_2',
+                'my_faster_rcnn_resnet152_v1_1024',
+                #'my_ssd_resnet50_v1_fpn_1024',
+                #'my_ssd_resnet101_v1_fpn_1024',
+                'my_centernet_hg104_512',
+                'my_centernet_hg104_1024',
+                #'my_centernet_hg104_1024_2',
+                #'my_centernet_hg104_1024_3',
+                'my_centernet_resnet101_v1_fpn_512',
+                'my_efficientdet_d1']
+                #'my_efficientdet_d4']
+                #'my_faster_rcnn_resnet101_v1_1024']
+                #'my_faster_rcnn_resnet101_v1_640']
+                #'my_faster_rcnn_resnet101_v1_1024_3']
 
 # models path
 models_path = os.path.join('Tensorflow','workspace','models')
+images_path = os.path.join('Tensorflow','workspace','images')
 
 
 def set_paths(model_name):
@@ -56,12 +66,13 @@ def get_evaluation_metrics(model_name, efile_path, event_dict):
             df = pd.DataFrame([[value.tag, event.step, t, type(t)]], columns=['Value', 'Step', 't', 't_type'])
             #print(value.tag, event.step, t, type(t))
             event_df = event_df.append(df, ignore_index=True)
-    print(event_df)
+    print(model_name, event_df, sep='\n')
     event_dict[model_name] = event_df
     return event_dict
 
-
+#######################
 # START PROGRAM
+#######################
 
 train_dict = {}
 eval_dict = {}
@@ -70,10 +81,58 @@ for model in custom_models:
     train_path, eval_path = set_paths(model)
     # train event files
     train_efile_path = get_event_file(train_path)
-    train_metrics_dict = get_evaluation_metrics(model,train_efile_path,train_dict)
+    train_dict = get_evaluation_metrics(model,train_efile_path,train_dict)
     # evaluation event files
     eval_efile_path = get_event_file(eval_path)
-    eval_metrics_dict = get_evaluation_metrics(model,eval_efile_path,eval_dict)
+    eval_dict = get_evaluation_metrics(model,eval_efile_path,eval_dict)
+
+# save dicts to excel
+def save_dict_to_excel(dict,path):
+    writer=pd.ExcelWriter(path, engine='openpyxl')
+    # save content of one key into one sheet
+    for model in dict.keys():
+        df = dict[model]
+        name = str(model)
+        # only 31 characters for sheet name allowed
+        if len(name) > 31:
+            count = len(name) - 31
+            # shorten name from the front
+            name = name[count:]
+        df.to_excel(writer, sheet_name=name)
+    writer.save()
+    writer.close()
+
+save_dict_to_excel(train_dict,"Train_Metrics_Dict.xlsx")
+save_dict_to_excel(eval_dict,"Eval_Metrics_Dict.xlsx")
+
+# compare precisions of custom models
+precision_fig = precision_plot(eval_dict)
+plt.savefig(os.path.join(models_path,"Precisions_Compare_allfromTUe.png"))
+plt.close()
+# compare recalls of custom models
+recall_ig = recall_plot(eval_dict)
+plt.savefig(os.path.join(models_path,"Recalls_Compare_allfromTUe.png"))
+plt.close()
+print("Precision and Recall plot saved under 'Workspace/Models'")
+
+# plot Training metrics
+tot_loss_dict = {}
+learn_rate_dict = {}
+for model in custom_models:
+    train_prop = train_dict[model]
+    tot_loss_dict[model] = train_prop[train_prop.Value=='Loss/total_loss']
+    learn_rate_dict[model] = train_prop[train_prop.Value=='learning_rate']
+    # plot and save total loss figure
+    plot_total_loss(tot_loss_dict[model],os.path.join(models_path,model))
+    # plot and save learning rate figure
+    plot_learningrate(learn_rate_dict[model],os.path.join(models_path,model))
+
+#launch_tensorboard(train_path)
+#launch_tensorboard(eval_path)
+# for view in debug console:
+#display(event_df)
+
+# edict_keys = list(eval_metrics_dict.keys())
 
 # TODO how to implement TIDE
 if 0:
@@ -81,20 +140,3 @@ if 0:
     tide.evaluate(datasets.COCO(), datasets.COCOResult(eval_efile_path), mode=TIDE.BOX)
     tide.summarize()  # Summarize the results as tables in the console
     tide.plot()       # Show a summary figure
-
-edict_keys = list(eval_metrics_dict.keys())
-
-precision_fig = precision_plot(eval_metrics_dict)
-plt.savefig(os.path.join(models_path,"Precisions_Compare.png"))
-recall_ig = recall_plot(eval_metrics_dict)
-plt.savefig(os.path.join(models_path,"Recalls_Compare.png"))
-
-#launch_tensorboard(train_path)
-#launch_tensorboard(eval_path)
-# for view in debug console:
-#display(event_df)
-
-# TODO for train_metrics:
-# separate df into Value.unique() to separate losses from each other
-
-x=1
